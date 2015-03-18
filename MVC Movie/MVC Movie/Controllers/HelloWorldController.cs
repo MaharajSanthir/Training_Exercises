e21435ebe56ff987e5040848551a9b6d2c7f5dd4
MaharajSanthir <msanthir@gmail.com>
2015-03-18 01:56:59 -0400

    Completed Tutorials

---
+++ MVC Movie/MVC Movie/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Movie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int ID = 1)
+        {
+            ViewBag.Message = string.Format("Hello {0}.", name);
+            ViewBag.NumTimes = ID;
+
+            return View();
+        }
+    }
+}
